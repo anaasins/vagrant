646d97663a06f97f4cc0687182707bea160ccc29
Ana Asins <asinsanna@gmail.com>
2017-12-11 08:57:11 +0100

    provision hasta git clone de calculadora

--- provision.sh
+++ provision.sh
@@ -1,2 +1,8 @@
 apt-get -y update
 apt-get -y install apache2
+cp /var/www/html/* /vagrant
+rm -r /var/www/html
+ln -s /vagrant /var/www/html
+apt-get install git
+cd /vagrant
+git clone https://github.com/anaasins/calculadora.git
